319bc6a788e97cca1340199936b3c8adc0a0eb49
Robert W. Leonard <robert@draknor.com>
2019-04-27 16:29:47 -0500

    Set up Redux flow

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import * as courseActions from "../../redux/actions/courseActions";
+import PropTypes from 'prop-types';
+import {bindActionCreators} from 'redux';
 
 class CoursesPage extends React.Component {
     
@@ -15,7 +19,7 @@ class CoursesPage extends React.Component {
 
     handleSubmit = event => {
         event.preventDefault();
-        alert(this.state.course.title);
+        this.props.actions.createCourse(this.state.course);
     }
 
     render() {
@@ -25,9 +29,29 @@ class CoursesPage extends React.Component {
                 <h3>Add Course</h3>
                 <input type="text" onChange={this.handleChange} value={this.state.course.title} />
                 <input type="submit" value="Save" />
+                { this.props.courses.map(course => (
+                   <div key={course.title}>{course.title}</div> 
+                ))}
             </form>
         );
     }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+    courses: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
+}
+
+function mapStateToProps(state) {
+    return {
+        courses: state.courses
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(courseActions, dispatch)
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,6 +1,8 @@
+import * as types from "../actions/actionTypes.js";
+
 export default function courseReducer(state = [], action) {
     switch(action.type) {
-        case "CREATE_COURSE":
+        case types.CREATE_COURSE:
             return [...state, { ...action.course}];
         
         default:
