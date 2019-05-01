a4e7ccdbd85a8b0c3626384a5182c970dbb6b3b3
Robert W. Leonard <robert@draknor.com>
2019-04-30 22:45:00 -0500

    Set up course navigation and form saving

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -5,8 +5,13 @@ import * as authorActions from '../../redux/actions/authorActions';
 import PropTypes from 'prop-types';
 import {bindActionCreators} from 'redux';
 import CourseList from './CourseList';
+import { Redirect } from "react-router-dom";
 
 class CoursesPage extends React.Component {
+    state = {
+        redirectToAddCoursePage: false
+    };
+
     componentDidMount() {
         const { courses, authors, actions } = this.props;
 
@@ -22,12 +27,16 @@ class CoursesPage extends React.Component {
                 alert("Loading authors failed " + error);
             });
         }
-
     }
     render() {
         return (
             <>
+                {this.state.redirectToAddCoursePage && <Redirect to="/course" />}
                 <h2>Courses</h2>
+
+                <button className="btn btn-primary mb-2 add-course" 
+                    onClick={() => this.setState({ redirectToAddCoursePage: true })}>Add Course</button>
+
                 <CourseList courses={this.props.courses} />
             </>
         );
--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-import { loadCourses } from "../../redux/actions/courseActions";
+import { loadCourses, saveCourse } from "../../redux/actions/courseActions";
 import { loadAuthors } from '../../redux/actions/authorActions';
 import PropTypes from 'prop-types';
 import CourseForm from "./CourseForm";
 import { newCourse } from "../../../tools/mockData";
 
-function ManageCoursePage({ courses, authors, loadAuthors, loadCourses, ...props }) {
+function ManageCoursePage({ courses, authors, loadAuthors, loadCourses, saveCourse, history, ...props }) {
     const [ course, setCourse ] = useState({...props.course});
     const [ errors, setErrors ] = useState({});
 
@@ -25,8 +25,25 @@ function ManageCoursePage({ courses, authors, loadAuthors, loadCourses, ...props
         }
     }, []);
 
+    function handleChange(event) {
+        const { name, value } = event.target;
+
+        setCourse( prevCourse => ({
+            ...prevCourse,
+            [name]: name === "authorId" ? parseInt(value, 10) : value
+        }));
+    }
+
+    function handleSave(event) {
+        event.preventDefault();
+
+        saveCourse(course).then( () => {
+            history.push("/courses");
+        });
+    }
+
     return (
-        <CourseForm course={course} errors={errors} authors={authors} />
+        <CourseForm course={course} errors={errors} authors={authors} onChange={handleChange} onSave={handleSave} />
     );
 }
 
@@ -35,7 +52,9 @@ ManageCoursePage.propTypes = {
     authors: PropTypes.array.isRequired,
     courses: PropTypes.array.isRequired,
     loadCourses: PropTypes.func.isRequired,
-    loadAuthors: PropTypes.func.isRequired
+    loadAuthors: PropTypes.func.isRequired,
+    saveCourse: PropTypes.func.isRequired,
+    history: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state) {
@@ -48,7 +67,8 @@ function mapStateToProps(state) {
 
 const mapDispatchToProps =  {
     loadCourses,
-    loadAuthors
+    loadAuthors,
+    saveCourse
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
